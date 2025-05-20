45146f8412e3dbe149870d2b04b9b512ccb4788f
vladislav kudinov <v.kudinov@dyn-it.de>
2025-05-20 08:09:26 +0000

    data change

--- srv/service.js
+++ srv/service.js
@@ -23,8 +23,13 @@ class ProcessorService extends cds.ApplicationService {
   /** Custom Validation */
   async onUpdate (req) {
     const { status_code } = await SELECT.one(req.subject, i => i.status_code).where({ID: req.data.ID})
-    if (status_code === 'C')
+    if (status_code === 'C'){
       return req.reject(`Can't modify a closed incident`)
+    }
+    let message = req.data.conversation[0].message;
+    req.data.conversation[0].message = '';
+    message = '';
+    message = '1';
   }
 }
 module.exports = { ProcessorService }
